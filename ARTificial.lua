9411e1d19a750311fcbf15af11a7bf8ea1bc0219
Marcel O'Neil <marcel@marceloneil.com>
2016-05-22 09:34:38 -0400

    API

--- ARTificial.lua
+++ ARTificial.lua
@@ -327,7 +327,7 @@ app.post('/submitTask', function(req, res)
     table.insert(styleFile, tostring(idnum) .. string.gsub(url, 'https://s3.amazonaws.com/artificial%-neural/', '/'))
   end
   for i = 0, #styleURL do
-    print(styleURL[i])
+    print(styleURL.1)
     local styleBody, styleCode = http.request(styleURL[i])
     if not styleBody then error(styleCode) end
     local sf = assert(io.open(styleFile[i], 'wb'))
