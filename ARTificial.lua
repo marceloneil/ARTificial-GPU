2488803fb2cfaaf29fdf0d83521f81af29414fda
Marcel O'Neil <marcel@marceloneil.com>
2016-05-22 11:01:28 -0400

    API

--- ARTificial.lua
+++ ARTificial.lua
@@ -305,7 +305,7 @@ end
 function uploadS3(name, id)
   local imgUpload = assert(io.open(name)):read("*all")
   print(bucket:put(name, imgUpload))
-  local reqbody = json.encode('{"idnum":' .. id .. ',"productImg":"https://s3.amazonaws.com/artificial-neural/"' .. name .. ',"recipient":"daniel_kt_wu@msn.com"}')
+  local reqbody = '{"idnum":' .. id .. ',"productImg":"https://s3.amazonaws.com/artificial-neural/"' .. name .. ',"recipient":"daniel_kt_wu@msn.com"}'
   print(reqbody)
   local result, respcode, respheaders, respstatus = http.request {
        method = "POST",
